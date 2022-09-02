f83ce636c8482b1b4e58b6638e3488ae44f73804
ginasekarangit <gunasekaranmech10@gmail.com>
2022-09-02 20:52:10 +0530

    my task

---
+++ src/org/tcs/Sample.java
@@ -0,0 +1,34 @@
+package org.tcs;
+
+public class Sample {
+	public static void main(String[] args) {
+		String a[]=new String[10];
+		a[0]="1";
+		a[1]="2";
+		a[2]="1";
+		a[3]="2";
+		a[4]="1";
+		a[5]="2";
+		a[6]="1";
+		a[7]="2";
+		a[8]="1";
+		a[9]="2";
+		 
+		for (int i = 0; i < 10; i++) {
+			System.out.println(a[i]);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+//		System.out.println(a[1]);
+//		int length = a.length;
+//		System.out.println(length);
+//	
+		}
+
+}
